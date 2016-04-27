22ef559f5dec8367e959b77333deca46742e1213
LozMarshall <lmarshall@salisbury6c.ac.uk>
2016-04-27 10:11:04 +0100

    tkinter

--- main3.py
+++ main3.py
@@ -34,13 +34,18 @@ class App(tk.Tk):
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="thermostat: ")
         label2 = tk.Label(self, text="heating: ")
         label3 = tk.Label(self, text="temperature: ")
 
+        button_quit = tk.Button(self, text="quit", command=self.quit)
+
         label.grid(row=0, column=0, pady=10, padx=10, sticky="w")
         label2.grid(row=1, column=0, pady=10, padx=10, sticky="w")
         label3.grid(row=2, column=0, pady=10, padx=10, sticky="w")
+
+        button_quit.grid(row=0, column=1, pady=10, padx=10, sticky="se")
 
 
 app = App()
