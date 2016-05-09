43d6bd5b35c481bc7e79b69634762dddf556ad50
LozMarshall <lmarshall@salisbury6c.ac.uk>
2016-05-09 11:23:26 +0100

    tkinter

--- main3.py
+++ main3.py
@@ -118,10 +118,12 @@ class HomePage(tk.Frame):
         button_page.grid(row=14, column=3, pady=10, padx=10, sticky="se")
         button_quit.grid(row=15, column=3, pady=10, padx=10, sticky="se")
 
-        #while True:
-         #   update_method(self)
+        update_method(self)
+        print("this is happpening")
+        sleep(0.2)
+        print("more is happening")
 
-        self.after(500, self.update_method)
+        #self.after(500, self.update_method)
 
     def update_method(self):
         sense = _SenseHat(rpi)
