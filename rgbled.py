ca06eb10d4603506e0b0ae266dd8abdd52d80458
LozMarshall <lmarshall@salisbury6c.ac.uk>
2016-05-16 11:31:14 +0100

    tkinter

--- rgbled.py
+++ rgbled.py
@@ -1,6 +1,6 @@
 class RGBLED:
     def __init__(self, board_object, red, green, blue):
-        self.GPIO = board_object
+        self.board = board_object
         self.red = red
         self.green = green
         self.blue = blue
@@ -8,30 +8,30 @@ class RGBLED:
 
     def setup(self):
         for pin in self.pins:
-            self.GPIO.setup(pin, self.GPIO.OUT)
+            self.board.GPIO.setup(pin, self.board.GPIO.OUT)
 
     def red_turn_on(self):
         self.clear()
-        self.GPIO.output(self.red, self.GPIO.HIGH)
+        self.board.GPIO.output(self.red, self.board.GPIO.HIGH)
         # return "RED LED ON"
 
     def green_turn_on(self):
         self.clear()
-        self.GPIO.output(self.green, self.GPIO.HIGH)
+        self.board.GPIO.output(self.green, self.board.GPIO.HIGH)
         # return "GREEN LED ON"
 
     def blue_turn_on(self):
         self.clear()
-        self.GPIO.output(self.blue, self.GPIO.HIGH)
+        self.board.GPIO.output(self.blue, self.board.GPIO.HIGH)
         # return "BLUE LED ON"
 
     def white_turn_on(self):
         self.clear()
-        self.GPIO.output(self.red, self.GPIO.HIGH)
-        self.GPIO.output(self.green, self.GPIO.HIGH)
-        self.GPIO.output(self.blue, self.GPIO.HIGH)
+        self.board.GPIO.output(self.red, self.board.GPIO.HIGH)
+        self.board.GPIO.output(self.green, self.board.GPIO.HIGH)
+        self.board.GPIO.output(self.blue, self.board.GPIO.HIGH)
         # return "WHITE LED ON"
 
     def clear(self):
         for pin in self.pins:
-            self.GPIO.output(pin, self.GPIO.LOW)
+            self.board.GPIO.output(pin, self.board.GPIO.LOW)
