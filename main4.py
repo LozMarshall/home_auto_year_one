6b7e2d88ee56ae082a910f057dea8d378d4804fc
LozMarshall <lmarshall@salisbury6c.ac.uk>
2016-05-20 09:36:18 +0100

    atmpt one to get RGB leds changing

--- main4.py
+++ main4.py
@@ -193,7 +193,7 @@ class HomePage(tk.Frame):
     def sense_led(self, state):
         # sense = _SenseHat(rpi)
         colour = [int(self.red_scale_val), int(self.green_scale_val), int(self.blue_scale_val)]
-        print(self.red_scale_val + " " + self.green_scale_val + " " + self.blue_scale_val)
+        print(colour)
         black = [0, 0, 0]
 
         if state == "on":
