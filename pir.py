0e57ae28b5c86d712d3a583705ddd0a3c25449e5
LozMarshall <lmarshall@salisbury6c.ac.uk>
2016-05-20 13:01:42 +0100

    First push of PIR class

---
+++ pir.py
@@ -0,0 +1,13 @@
+class PIR:
+    def __init__(self, board_object, pin):
+        self.__board = board_object
+        self.pin = pin
+        self.setup()
+
+    def setup(self):
+        self.__board.GPIO.setup(self.pin, self.__board.GPIO.IN)
+
+    def motion_detect(self):
+        if self.__board.GPIO.input(self.pin):
+            return "Motion detected"
+
