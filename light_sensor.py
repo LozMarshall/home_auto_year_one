3fb52bb4cb51a36911cfd1354e404809aea995a3
LozMarshall <lmarshall@salisbury6c.ac.uk>
2016-05-23 12:08:28 +0100

    added LDR functionality, untested

--- light_sensor.py
+++ light_sensor.py
@@ -12,9 +12,32 @@ class LDR:
         self.__board.GPIO.setup(self.pin, self.__board.GPIO.OUT)
         self.__board.GPIO.output(self.pin, self.__board.GPIO.LOW)
         sleep(0.1)
-        self.__board.GPIO.setup(self.pin, self.__board.GPIO.IN)
+
 
     def sensor_read(self):
+        self.__board.GPIO.setup(self.pin, self.__board.GPIO.IN)
         if self.__board.GPIO.input(self.pin) == self.__board.GPIO.LOW:
             self.reading += 1
         return self.reading
+
+
+#
+# import time
+# import RPi.GPIO as GPIO
+#
+# GPIO.setmode(GPIO.BCM)
+#
+#
+# def rc_time(rc_pin):
+#     reading = 0
+#     GPIO.setup(rc_pin, GPIO.OUT)
+#     GPIO.output(rc_pin, GPIO.LOW)
+#     time.sleep(0.1)
+#
+#     GPIO.setup(rc_pin, GPIO.IN)
+#     while GPIO.input(rc_pin) == GPIO.LOW:
+#         reading += 1
+#     return reading
+#
+# while True:
+#     print(rc_time(24))
