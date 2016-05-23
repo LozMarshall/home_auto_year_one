730fefbaa273040aa37034fa71f274834633b2cd
LozMarshall <lmarshall@salisbury6c.ac.uk>
2016-05-23 12:45:02 +0100

    LDR class completed

--- light_sensor.py
+++ light_sensor.py
@@ -5,10 +5,9 @@ class LDR:
     def __init__(self, board_object, pin):
         self.__board = board_object
         self.pin = pin
-        self.reading = 0
 
     def sensor_read(self):
-        self.reading = 0
+        reading = 0
         self.__board.GPIO.setup(self.pin, self.__board.GPIO.OUT)
         self.__board.GPIO.output(self.pin, self.__board.GPIO.LOW)
 
@@ -16,5 +15,5 @@ class LDR:
 
         self.__board.GPIO.setup(self.pin, self.__board.GPIO.IN)
         while self.__board.GPIO.input(self.pin) == self.__board.GPIO.LOW:
-            self.reading += 1
-        return self.reading
+            reading += 1
+        return reading
