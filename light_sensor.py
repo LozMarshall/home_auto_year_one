5ac0842c2256695e195cc98bcce49478750deae9
LozMarshall <lmarshall@salisbury6c.ac.uk>
2016-05-23 12:22:02 +0100

    added LDR functionality, untested

--- light_sensor.py
+++ light_sensor.py
@@ -6,16 +6,15 @@ class LDR:
         self.__board = board_object
         self.pin = pin
         self.reading = 0
-        self.setup()
-
-    def setup(self):
-        self.__board.GPIO.setup(self.pin, self.__board.GPIO.OUT)
-        self.__board.GPIO.output(self.pin, self.__board.GPIO.LOW)
-        sleep(0.1)
-        self.__board.GPIO.setup(self.pin, self.__board.GPIO.IN)
 
     def sensor_read(self):
-        if self.__board.GPIO.input(self.pin) == self.__board.GPIO.LOW:
+        self.__board.GPIO.setup(self.pin, self.__board.GPIO.OUT)
+        self.__board.GPIO.output(self.pin, self.__board.GPIO.LOW)
+
+        sleep(0.1)
+
+        self.__board.GPIO.setup(self.pin, self.__board.GPIO.IN)
+        while self.__board.GPIO.input(self.pin) == self.__board.GPIO.LOW:
             self.reading += 1
         return self.reading
 
